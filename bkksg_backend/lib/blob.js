a54cc7a6edc32394ed6ca06ff4b870d780ccf9c5
poetdeveloper <poetdeveloper.iro@gmail.com>
2022-05-13 17:52:40 +0900

    before Deploy

--- bkksg_backend/lib/blob.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-     send : file =>{
-          axios.post('/admin/axios_process', {file})
-          .then('Suceess')
-          .catch(console.log);
-     },
-}
--- bkksg_backend/routes/index.js
+++ bkksg_backend/routes/index.js
@@ -4,12 +4,13 @@ const db = require("../lib/db");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-         db.query(
-          `SELECT content.id,content.title,content.description,topic,cover_src FROM content LEFT JOIN type ON content.type_id = type.id WHERE public = 1 ORDER BY content.id DESC`,
-            (err1, contents) => {
-          if (err1) throw err1;
-            res.status(200).render('index', {contents});
-          });
+  db.query(
+    `SELECT content.id,content.title,content.description,topic,cover_src FROM content LEFT JOIN type ON content.type_id = type.id WHERE public = 1 ORDER BY content.id DESC`,
+    (err1, contents) => {
+      if (err1) throw err1;
+      res.status(200).render('index', {contents});
+      console.log('DATA'+contents[0].title)
+    });
 });
 
 module.exports = router;
