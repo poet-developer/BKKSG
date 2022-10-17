cee1d90865db65b01f28c55cad3f4416d65e34fe
poetdeveloper <poetdeveloper.iro@gmail.com>
2022-10-17 15:08:26 +0900

    done

--- bkksg_nextjs/pages/_app.js
+++ bkksg_nextjs/pages/_app.js
@@ -17,6 +17,7 @@ import Footer from "../src/Component/UI/Footer"
 import Sidebar from "../src/Component/UI/Sidebar"
 import HeadMeta from "../src/Component/lib/SEO"
 import Head from "next/head";
+import { NextSeo } from 'next-seo';
 
 function MyApp(
   { Component, pageProps }
@@ -40,23 +41,40 @@ function MyApp(
   
   return (
     <ThemeProvider theme={themeMode ? theme.night : theme.day}>
+      {/* <NextSeo
+        title="비껴서기 | BKKSG"
+				description="비껴서기 | BKKSG : 林이로가 비껴서서 생각하고 설계하고 만드는, 움직이는 화랑 | 글, 시각, 설치, 기획"
+				openGraph={{
+					type: 'website',
+					url: 'http://bkksg.com/',
+					title: '비껴서기 | BKKSG',
+					description: '비껴서기 | BKKSG : 林이로가 비껴서서 생각하고 설계하고 만드는, 움직이는 화랑 | 글, 시각, 설치, 기획',
+					images: [
+						{
+							url: './thumbnail/bkksg_thumbnail.png',
+							width: 1200,
+							height: 630,
+						},
+					],
+				}}
+			/> */}
     <Head>
     <meta property="og:type" content="website" />
+        <meta property="og:url" content={`http://bkksg.com/`} key='og-url' />
         <meta property="og:title" content={`비껴서기 | BKKSG`} key='og-title'/>
         <meta property="og:site_name" content={`비껴서기 | BKKSG`} key='og-site' />
         <meta property="og:description" content={`비껴서기 | BKKSG : 林이로가 비껴서서 생각하고 설계하고 만드는, 움직이는 화랑 | 글, 시각, 설치, 기획`} key='og-desc'/>
-        <meta property="og:image" content={'http://bkksg.com/thumbnail/bkksg_thumbnail.png' } key='og-image' />
-        <meta property="og:url" content={`http://bkksg.com/`} key='og-url' />
+        <meta property="og:image" content={'http://bkksg.com/thumbnail/bkksg_thumbnail.png'} key='og-image' />
         <meta property="og:image:width" content="1200"/>
         <meta property="og:image:height" content="630"/>
         <meta name="twitter:card" content="summary"/>
         <meta name="twitter:title" content={`비껴서기 | BKKSG`} />
         <meta name="twitter:description" content={`비껴서기 | BKKSG : 林이로가 비껴서서 생각하고 설계하고 만드는, 움직이는 화랑 | 글, 시각, 설치, 기획`}/>
-        <meta name="twitter:image" content={'http://bkksg.com/thumbnail/bkksg_thumbnail.png' }/>
+        <meta name="twitter:image" content={'http://bkksg.com/thumbnail/bkksg_thumbnail.png'}/>
         <meta name="naver-site-verification" content={process.env.NEXT_PUBLIC_REACT_NAVER_SITE_VERIFICATION} />
 
         <link rel="canonical" href={`http://bkksg.com/`} />
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="http://bkksg.com/favicon.ico"/>
     </Head>
     <div className="grid-container">
     <Header
