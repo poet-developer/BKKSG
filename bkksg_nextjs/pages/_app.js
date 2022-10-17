d8eefa88923de97aaad9bd97ccb0b3a68c98b40d
poetdeveloper <poetdeveloper.iro@gmail.com>
2022-10-17 15:42:13 +0900

    done

--- bkksg_nextjs/pages/_app.js
+++ bkksg_nextjs/pages/_app.js
@@ -17,7 +17,7 @@ import Footer from "../src/Component/UI/Footer"
 import Sidebar from "../src/Component/UI/Sidebar"
 import HeadMeta from "../src/Component/lib/SEO"
 import Head from "next/head";
-import { NextSeo } from 'next-seo';
+import { DefaultSeo } from "next-seo";
 
 function MyApp(
   { Component, pageProps }
@@ -37,42 +37,38 @@ function MyApp(
     setThemeMode(!themeMode)
   };
 
+  const DEFAULT_SEO = {
+    title: "비껴서기 | BKKSG",
+    description: "비껴서기 | BKKSG : 林이로가 비껴서서 생각하고 설계하고 만드는, 움직이는 화랑 | 글, 시각, 설치, 기획",
+    canonical: "http://bkksg.com/",
+    openGraph: {
+      type: "website",
+      locale: "ko_KR",
+      url: "http://bkksg.com/",
+      title: "비껴서기 | BKKSG",
+      site_name: "비껴서기",
+      images: [
+        {
+          url: "http://bkksg.com/thumbnail/bkksg_thumbnail.png",
+          width: 1200,
+          height: 630,
+          alt: "BKKSG"
+        }
+      ]
+    },
+    twitter: {
+        handle: '@bkksgstudio',
+        site: '@bkksg.com',
+        cardType: 'summary_large_image',
+    },
+  };
+  
+
   if (!componentMounted) return <div />
   
   return (
     <ThemeProvider theme={themeMode ? theme.night : theme.day}>
-      {/* <NextSeo
-        title="비껴서기 | BKKSG"
-				description="비껴서기 | BKKSG : 林이로가 비껴서서 생각하고 설계하고 만드는, 움직이는 화랑 | 글, 시각, 설치, 기획"
-				openGraph={{
-					type: 'website',
-					url: 'http://bkksg.com/',
-					title: '비껴서기 | BKKSG',
-					description: '비껴서기 | BKKSG : 林이로가 비껴서서 생각하고 설계하고 만드는, 움직이는 화랑 | 글, 시각, 설치, 기획',
-					images: [
-						{
-							url: './thumbnail/bkksg_thumbnail.png',
-							width: 1200,
-							height: 630,
-						},
-					],
-				}}
-			/> */}
-    <Head>
-    <title>{'비껴서기 | BKKSG'}</title>
-      <meta
-        name="description"
-        content={
-          '비껴서기 | BKKSG : 林이로가 비껴서서 생각하고 설계하고 만드는, 움직이는 화랑 | 글, 시각, 설치, 기획'
-        }
-      />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta property="og:title" content={'비껴서기 | BKKSG'} />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={"http://bkksg.com/"} />
-      <meta property="og:image" content='http://bkksg.com/thumbnail/bkksg_thumbnail.png'/>
-      <meta property="og:article:author" content="bkksg" />
-    </Head>
+    <DefaultSeo {...DEFAULT_SEO} />
     <div className="grid-container">
     <Header
       themeMode={themeMode}
