380b6a03ddfa4e32a8cf5910d688790d7ed9e344
poetdeveloper <poetdeveloper.iro@gmail.com>
2022-10-18 21:43:53 +0900

    SEO

--- bkksg_nextjs/next.config.js
+++ bkksg_nextjs/next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+
+  images: {
+    domains: ['https://bkksg-images.s3.ap-northeast-2.amazonaws.com/ProjectImages_highquality/bkksg_thumbnail.png'],
+  },
   
 }
 
